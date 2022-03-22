5dfbb0bf031685dbb298e620b19c184cac96e859
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-03-21 19:36:09 -0500

    added more info

--- variable.tf
+++ variable.tf
@@ -1,8 +1,21 @@
-variable "cidr_block" {}
-variable "tags" {
-    type = map
+variable "cidr_block" {
+    description = "Please provide a CIDR block to build a VPC"
+    type = string 
+    default = "10.0.0.0/16"
 }
-
+variable "tags" {
+    description = "Please provide a TAG  to build a VPC"
+    type = map
+    default = {
+        Name = "Dev" 
+    }
+}
 variable "private_subnets_cidr" {
+    description = "Please provide a CIDR block for private subnets  to build a VPC"
     type = list
+    default = [
+        "10.0.1.0/24",
+        "10.0.2.0/24",
+        "10.0.3.0/24",
+    ]
 }
